39101248902a634c98117b5c257bb6e1b1cac6af
Bulut Can Göçer <116563960+bulutcan99@users.noreply.github.com>
2025-08-02 22:07:03 +0300

    Delete vectors/target directory

--- vectors/target/debug/deps/vectors-6c2dd06f7aa8734d.d
+++
@@ -1,5 +0,0 @@
-/Users/bulutgocer/PROJECTS/RUST/github/bulutcan99/mini-rust/vectors/target/debug/deps/vectors-6c2dd06f7aa8734d: src/main.rs
-
-/Users/bulutgocer/PROJECTS/RUST/github/bulutcan99/mini-rust/vectors/target/debug/deps/vectors-6c2dd06f7aa8734d.d: src/main.rs
-
-src/main.rs:
--- vectors/target/debug/vectors.d
+++
@@ -1,1 +0,0 @@
-/Users/bulutgocer/PROJECTS/RUST/github/bulutcan99/mini-rust/vectors/target/debug/vectors: /Users/bulutgocer/PROJECTS/RUST/github/bulutcan99/mini-rust/vectors/src/main.rs
