9e993eccdee659d8be3d6cd8f3b6a6667f6d47e5
bulutcan99 <gocerbulutcan@gmail.com>
2025-08-04 22:52:33 +0300

    feat: started to impl grow

--- myvec/src/lib.rs
+++ myvec/src/lib.rs
@@ -7,6 +7,11 @@ mod tests {
     #[test]
     fn it_works() {
         let vec_ex = Vec::<u8>::new();
-        let result = myvec::MyVec::<u8>::default();
+        let mut result = myvec::MyVec::<u8>::default();
+        println!("Cap: {:?}", result.get_capacity());
+        println!("PTR: {:?}", result.get_ptr());
+        result.grow();
+        println!("New Cap: {:?}", result.get_capacity());
+        println!("New PTR: {:?}", result.get_ptr());
     }
 }
--- myvec/src/myvec.rs
+++ myvec/src/myvec.rs
@@ -1,4 +1,7 @@
-use std::ptr::NonNull;
+use std::{
+    alloc::{self, GlobalAlloc, Layout},
+    ptr::NonNull,
+};
 
 pub struct MyVec<T> {
     pub pointer: NonNull<T>,
@@ -13,5 +16,39 @@ impl<T> Default for MyVec<T> {
             length: 0,
             capacity: 0,
         }
+    }
+}
+
+impl<T> MyVec<T> {
+    pub fn get_capacity(&self) -> u8 {
+        self.capacity
+    }
+
+    pub fn grow(&mut self) {
+        if self.capacity == 0 {
+            self.capacity = 1;
+        }
+
+        self.capacity *= 2;
+        self.alloc_impl();
+    }
+
+    pub fn get_ptr(&self) -> NonNull<T> {
+        self.pointer
+    }
+
+    fn alloc_impl(&mut self) {
+        let layout = Layout::array::<T>(self.capacity as usize).unwrap();
+
+        let raw_ptr: *mut u8 = unsafe {
+            if self.get_capacity() == 0 {
+                alloc::alloc(layout)
+            } else {
+                let old_layout = Layout::array::<T>(self.length as usize).unwrap();
+                alloc::realloc(self.pointer.as_ptr() as *mut u8, old_layout, layout.size())
+            }
+        };
+
+        self.pointer = NonNull::new(raw_ptr as *mut T).expect("Failed to allocate memory");
     }
 }
