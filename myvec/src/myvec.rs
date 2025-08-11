86482d78e47eda6a158a7798aa6ed2da80694a40
bulutcan99 <gocerbulutcan@gmail.com>
2025-08-11 21:32:40 +0300

    grow fixed

--- myvec/src/myvec.rs
+++ myvec/src/myvec.rs
@@ -24,31 +24,45 @@ impl<T> MyVec<T> {
         self.capacity
     }
 
-    pub fn grow(&mut self) {
-        if self.capacity == 0 {
-            self.capacity = 1;
-        }
+    fn set_capacity(&mut self, new_cap: u8) -> u8 {
+        self.capacity = new_cap;
+        self.capacity
+    }
 
-        self.capacity *= 2;
-        self.alloc_impl();
+    pub fn get_pointer(&self) -> NonNull<T> {
+        self.pointer
+    }
+
+    fn set_pointer(&mut self, new_ptr: NonNull<T>) {
+        self.pointer = new_ptr;
+    }
+
+    pub fn grow(&mut self) {
+        let old_cap = self.get_capacity();
+        let new_cap = if old_cap == 0 { 1 } else { old_cap * 2 };
+
+        let new_layout = Layout::array::<T>(new_cap as usize).unwrap();
+
+        let raw_ptr = if old_cap == 0 {
+            unsafe { alloc::alloc(new_layout) }
+        } else {
+            let old_layout = Layout::array::<T>(old_cap as usize).unwrap();
+            unsafe {
+                alloc::realloc(
+                    self.get_pointer().as_ptr() as *mut u8,
+                    old_layout,
+                    new_layout.size(),
+                )
+            }
+        };
+
+        let new_ptr = NonNull::<T>::new(raw_ptr as *mut T).expect("Failed to allocate memory");
+
+        self.set_pointer(new_ptr);
+        self.set_capacity(new_cap);
     }
 
     pub fn get_ptr(&self) -> NonNull<T> {
         self.pointer
-    }
-
-    fn alloc_impl(&mut self) {
-        let layout = Layout::array::<T>(self.capacity as usize).unwrap();
-
-        let raw_ptr: *mut u8 = unsafe {
-            if self.get_capacity() == 0 {
-                alloc::alloc(layout)
-            } else {
-                let old_layout = Layout::array::<T>(self.length as usize).unwrap();
-                alloc::realloc(self.pointer.as_ptr() as *mut u8, old_layout, layout.size())
-            }
-        };
-
-        self.pointer = NonNull::new(raw_ptr as *mut T).expect("Failed to allocate memory");
     }
 }
